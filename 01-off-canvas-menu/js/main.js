b5ef11a5847ae7f8b0249d77f79d48decf589580
acsixtysix <acamarian66@gmail.com>
2019-08-14 22:54:30 +0000

    refactored the event listener

--- 01-off-canvas-menu/js/main.js
+++ 01-off-canvas-menu/js/main.js
@@ -1,6 +1,9 @@
 const toggleCta = document.querySelector(".js-toggle-off-canvas");
 const page = document.querySelector(".js-page");
 
-toggleCta.addEventListener("click", function() {
+const toggleOffCanvas = _ => {
+  // underscore due to no argument in the arrow function
   page.classList.toggle("off-canvas-is-open");
-});
+};
+
+toggleCta.addEventListener("click", toggleOffCanvas);
